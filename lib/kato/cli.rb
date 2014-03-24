6c882c3e6e11a93ce8535c2307f236174299c3c9
Tom McDonald <tommcdo@gmail.com>
2014-03-23 21:26:17 -0400

    Use proper code renderer as per new Kato version

--- lib/kato/cli.rb
+++ lib/kato/cli.rb
@@ -4,10 +4,6 @@ require 'kato'
 module Kato
   module Cli
     def Cli.post(user, message, room, options)
-      if options[:renderer] == :code
-        options[:renderer] = :markdown
-        message = "```\n#{message}\n```"
-      end
       room = Kato::Room.new(room)
       room.post(user, message, options)
     end
